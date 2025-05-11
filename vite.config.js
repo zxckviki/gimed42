f63d9f09e4f28d623ee122d2d18ef5601a41547a
Alexey Rice <raysaleksey@gmail.com>
2025-05-11 22:20:44 +0700

    Add SPA redirects

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
-  base: '/', //  Важно! Базовый URL (измените, если деплой в подпапку)
+  base: '/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
-  // Опционально: настройки для сервера разработки
   server: {
-    historyApiFallback: true //  Для vue-router (чтобы все пути вели на index.html)
+    historyApiFallback: true,
   },
-  // Опционально: настройки сборки
   build: {
-    outDir: 'dist', // Папка для результатов сборки
-    assetsDir: 'assets', // Папка для статики (css/js/img)
-    emptyOutDir: true // Очищать папку перед сборкой
-  }
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
 })
