c72f7b878ca9cc16c4531f88032e384735e610de
Alexey Rice <raysaleksey@gmail.com>
2025-05-13 18:49:08 +0700

    Add SPA redirects

--- src/main.js
+++ src/main.js
@@ -13,16 +13,31 @@ import Privacy from './pages/Privacy.vue'
 const app = createApp(App)
 
 const routes = [
-  { path: '/catalog', name: 'Home', component: Home },
+  {
+    path: '/catalog',
+    name: 'Home',
+    component: Home,
+    meta: { title: 'Каталог продуктов | ООО "Гимед"' },
+  },
   // { path: '/favorites', name: 'Favorites', component: Favorites },
-  { path: '/privacy', name: 'Privacy', component: Privacy },
-  { path: '/', name: 'Landing', component: Landing, meta: { noLayout: true }}
+  {
+    path: '/privacy',
+    name: 'Privacy',
+    component: Privacy,
+    meta: { title: 'Политика конфиденциальности | ООО "Гимед"' },
+  },
+  { path: '/', name: 'Landing', component: Landing, meta: { title: 'Главная | ООО "Гимед"' } },
 ]
-
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
 })
 
 app.use(router)
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,34 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'ГИМЕД',
+        short_name: 'ГИМЕД',
+        theme_color: '#08546a',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   base: '/',
   resolve: {
     alias: {
